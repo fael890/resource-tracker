46876b833fe8e8a1ad2ba2b540ab2be9b3b9a796
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-02-04 16:33:24 -0300

    feat: base project

---
+++ src/main/java/com/rafa/resourcetracker/ResourcetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rafa.resourcetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafa/resourcetracker/ResourcetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafa.resourcetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
