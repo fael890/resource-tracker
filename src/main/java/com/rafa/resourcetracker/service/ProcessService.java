67333e459997bee57f3d46b7b7c8a2ca470f70eb
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-02-13 20:18:41 -0300

    feat: add CPU percentage calculation, but it needs refactor

--- src/main/java/com/rafa/resourcetracker/service/ProcessService.java
+++ src/main/java/com/rafa/resourcetracker/service/ProcessService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.rafa.resourcetracker.dto.ProcessDTO;
 import com.rafa.resourcetracker.entity.Process;
 import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
 import oshi.software.os.OSProcess;
 import oshi.software.os.OperatingSystem;
 import oshi.software.os.OperatingSystem.ProcessSorting;
@@ -14,24 +15,37 @@ import oshi.software.os.OperatingSystem.ProcessSorting;
 public class ProcessService {
     private SystemInfo systemInfo = new SystemInfo();
     private OperatingSystem os = systemInfo.getOperatingSystem();
+    private CentralProcessor processor = systemInfo.getHardware().getProcessor();
+    //(process.getProcessCpuLoadCumulative()*100d)/processor.getLogicalProcessorCount(),
 
     public List<ProcessDTO> getProcessList(){
         List<OSProcess> osProcessList = os.getProcesses(null, ProcessSorting.CPU_DESC, 10);
         List<ProcessDTO> processList;
         
         processList = osProcessList.stream()
-        .map(process -> new Process(
-            process.getProcessID(), 
-            process.getName(), 
-            process.getProcessCpuLoadCumulative(),
-            0.0,
-            process.getResidentSetSize(),
-            0.0,
-            0.0,
-            null
-        ))
+        .map(
+            process -> new Process(
+                process.getProcessID(), 
+                process.getName(),
+                (process.getProcessCpuLoadBetweenTicks(process)*100d)/processor.getLogicalProcessorCount(),
+                0.0,
+                process.getResidentSetSize(),
+                0.0,
+                0.0,
+                null
+            )
+        )
         .map(entity -> new ProcessDTO(entity)).toList();
 
         return processList;
     }
+
+    // public OSProcess calculateProcessCpuUsage(OSProcess process){
+    //     int cpuNumber = processor.getLogicalProcessorCount();
+    //     double previousTime = 0;
+
+    //     double currentTime = process.getKernelTime() + process.getUserTime();
+    //     double timeDifference = currentTime - previousTime;
+	// 	double cpu = (100d * (timeDifference / ((double) 1000))) / cpuNumber;
+    // }
 }
